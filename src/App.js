960f49151c92d406398e9aa4587e0a3c02c04274
someuserABC <someuserABC@gmail.com>
2020-08-25 17:08:11 +0300

    replace

--- src/App.js
+++ src/App.js
@@ -1,58 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import styles from "./App.module.css";
+import Content from "./components/content/Content";
+import Footer from "./components/footer/Footer";
+import Header from "./components/header/Header";
+import Spinner from "./components/_common/Spinner";
+import { aboutMe, getIsInit } from "./store/init-slice";
 
-function App() {
+const App = () => {
+  // console.log("App");
+  // Перерисовка происходит 2а раза (1-ый при получении, 2-ой при изменении)
+  // при этом перерисовка происходит после инициализации переменной, т.е.
+  // console.log(isInit) после isInit не будет undefined
+  const isInit = useSelector(getIsInit);
+
+  // dispatch мемоизируется по умолчанию (пригоден для использования в useEffect)
+  const dispatch = useDispatch();
+
+  // useEffect вызывает перерендер, даже при вызове пустого коллбека
+  useEffect(() => {
+    dispatch(aboutMe(true));
+  }, [dispatch]);
+
+  if (!isInit) {
+    return (
+      <div className={styles["app"]}>
+        <Spinner />
+      </div>
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+    <div className={styles["app"]}>
+      <Header />
+      <Content />
+      <Footer />
     </div>
   );
-}
+};
 
 export default App;
